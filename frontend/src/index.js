f2050da56f8985418f52e32c4db9720d8f04ea6b
ModdedXero <53191205+ModdedXero@users.noreply.github.com>
2021-01-26 10:36:28 -0700

    Removed Snap temp

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,23 +3,11 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
 import App from './components/App';
 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  ReactDOM.hydrate(
-    <React.StrictMode>
-      <Router>
-        <App />
-      </Router>
-    </React.StrictMode>,
-    rootElement
-  );
-} else {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Router>
-        <App />
-      </Router>
-    </React.StrictMode>,
-    rootElement
-  );
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
